427ba7a0e13a407a27419a3db971605939709a58
Jessica <jessicac@student.dei.uc.pt>
2022-09-03 16:58:55 +0100

    cleaned code

--- sge/sge/operators/recombination.py
+++ sge/sge/operators/recombination.py
@@ -13,15 +13,6 @@ def crossover(p1, p2):
         else:
             genotype.append(p2['genotype'][index][:])
     mapping_values = [0] * gen_size
-    # check parent with highest fitness
-    if params['METHOD'] == 2:
-        # nota - pego na gramatica do melhor individuo
-        if p1['fitness'] < p2['fitness']:
-            gram = p1['grammar']
-        else:
-            gram = p2['grammar']
-    else:
-        gram = grammar.get_dict()
     # compute nem individual
-    _, tree_depth = grammar.mapping(genotype, gram, mapping_values)
-    return {'genotype': genotype, 'fitness': None, 'mapping_values': mapping_values, 'tree_depth': tree_depth, 'grammar': gram}
+    _, tree_depth = grammar.mapping(genotype, mapping_values)
+    return {'genotype': genotype, 'fitness': None, 'mapping_values': mapping_values, 'tree_depth': tree_depth}
