b6461e8aa89118519c1ddd1cd8a313f123f49d31
swom <cicciacco@gmail.com>
2023-09-18 12:21:31 +0200

    adding scripts for cluster

--- sge/sending_trillions.sh
+++ sge/sending_trillions.sh
@@ -11,4 +11,4 @@ pip install numpy
 pip install PyYAML
 pip install tqdm
 
-python -m examples.symreg --experiment_name /scratch/p288427/megalomania/first_run --seed 71920 --parameters mut_params/mut_level_1.0_0.0025.yml --grammars/regression.pybnf --gauss_sd $1 --prob_mutation_probs $2 --mutate_grammar $3
+python -m examples.symreg --experiment_name /project/p288427/megalomania/first_run --seed 71920 --parameters parameters/standard.yml --grammars/regression.pybnf --gauss_sd $1 --prob_mutation_probs $2 --mutate_grammar $3
