f8235daacfaf0cc8b66c5bd71c9a979e1b86f99c
swom <cicciacco@gmail.com>
2023-10-24 16:15:09 +0200

    updating script for plotting

--- sge/script for plotting.R
+++ sge/script for plotting.R
@@ -1,43 +1,65 @@
 library(readr)
+library(plyr)
 library(dplyr)
-library(data.table)
+library(ggbeeswarm)
 library(ggplot2)
-library(patchwork)
 library(ggpubr)
-wd = "C:/Users/p288427/Desktop/megalomania/psge/"
+library(patchwork)
+library(viridis)
+wd = "C:/Users/p288427/Desktop/megalomania/"
+setwd(wd)
+
 
 #read and save the data
 read_and_save = function(dir){
   setwd(dir)
   dirs = list.dirs()
   paths =  dirs[grepl("*run_\\d+", dirs)][-1]
-  data = data.table()
+  data = data.frame()
   
   for (path in paths) {
-    
     cat(path)
-    
     filename = file.path(path,"progress_report.csv")
-    # Read the file contents as a character vector
-    file_contents <- read_file(filename)
-    # Substitute corrupted newlines followed by a space with just a newline
-    cleaned_contents <- gsub("[\r\n]\\s", "\\s", file_contents)
-    tempfile <- tempfile()
-    writeLines(cleaned_contents, tempfile)
-    
-    # Read the CSV file from the temporary file
-    tmp_data =  read.csv(tempfile, 
-                         header = F,
-                         sep = ";")
     
     properties <- read.table(text = path, sep = "/")[-1]
-    colnames(properties) <- sub('_[^_]*$', '', properties)
-    properties[1, ] <- sub('.*\\_', '', properties)
+    colnames(properties) <- c("alg_type",sub('_[^_]*$', '', properties[-1]))
+    properties[1, -1] <- sub('.*\\_', '', properties[-1])
+    if(!"remap" %in% colnames(properties)){
+      properties = properties %>% mutate(remap = "False")
+    } 
+    
+    tmp_data = data.frame(matrix(ncol = 9, nrow = 0))
+    
+    if (properties$alg_type == "psge") {
+      
+      
+      # Read the file contents as a character vector
+      file_contents <- read_file(filename)
+      # Substitute corrupted newlines followed by a space with just a newline
+      cleaned_contents <- gsub("[\r\n]\\s", "\\s", file_contents)
+      tempfile <- tempfile()
+      writeLines(cleaned_contents, tempfile)
+      # Read the CSV file from the temporary file
+      tmp_data = read.csv(tempfile, 
+                          header = F,
+                          sep = ";")
+      colnames(tmp_data)[1:9]=c("gen","best_fit","genot","phenot","mut_prob","gram","fit_mean","fit_sd","best_error_test")
+      
+      
+      
+    } else  {
+      colnames(tmp_data)[1:9]=c("gen","best_fit","genot","phenot","mut_prob","gram","fit_mean","fit_sd","best_error_test")
+      csv = read.csv2(filename, 
+                     header = F,
+                     col.names = c("gen","best_fit","fit_mean","fit_sd"),
+                     sep = "\t")
+      tmp_data = rbind.fill(tmp_data, csv)
+      
+    }
     
     data = rbind(data,cbind(tmp_data,properties))
     print(" ->done")
   }
-  colnames(data)[1:9]=c("gen","best_fit","genot","phenot","mut_prob","gram","fit_mean","fit_sd","best_error_test")
   
   write.csv(data, file = "data.csv")
   return(data)
@@ -50,13 +72,9 @@ fitness_over_time_across_approachs <- function(data){
               aes(x = gen, 
                   y = best_fit,
                   color = interaction(start_mut_prob, prob_mut_probs, gauss, remap, delay),
-                  fill = interaction(start_mut_prob, prob_mut_probs, gauss, remap, delay),
-                  alpha = 0.0001)
-  )+
-    geom_line(linewidth = 0.1,
-              linetype = "dotted",
-              aes(group = interaction(start_mut_prob, prob_mut_probs, gauss, remap, delay, run))
-              ) +
+                  fill = interaction(start_mut_prob, prob_mut_probs, gauss, remap, delay)
+                  )
+  ) +
     scale_color_viridis(discrete = T) +
     scale_fill_viridis(discrete = T) +
     # ylim(c(0, 1))+
@@ -67,20 +85,23 @@ fitness_over_time_across_approachs <- function(data){
   q = p + geom_smooth(
     aes(group = interaction(start_mut_prob, prob_mut_probs, gauss, remap, delay))
   )
-
+  
   z = p + 
     geom_line(linewidth = 0.1,
               linetype = "dotted",
               aes(group = interaction(start_mut_prob, prob_mut_probs, gauss, remap, delay, run))
     ) + 
-    facet_grid(cols = vars(start_mut_prob, prob_mut_probs, gauss, remap, delay)) +
+    + geom_smooth(
+      aes(group = interaction(start_mut_prob, prob_mut_probs, gauss, remap, delay))
+    )
+    facet_grid(cols = rev(vars(start_mut_prob, prob_mut_probs, gauss, remap, delay))) +
     theme(legend.position = "None")
- 
+  
   layout = 'AAAA
             AAAA
             BBBB'
-
-  x = q / z #+ plot_annotation(title = "Average performance of solutions over evolution")
+  
+  x = q / z + plot_annotation(title = "Average performance of solutions over evolution")
   print(x + plot_layout(guides = 'collect', design = layout))
   ggsave("fitness_over_time_per_run_across_approachs_details.pdf",
          width = 14,
@@ -89,22 +110,13 @@ fitness_over_time_across_approachs <- function(data){
   return(x)
 }
 
-### grouping is done by "approach"
+### add violin plots with nice aestethics
 add_violin_box_plot<- function(plot){
   return(
     plot +
       geom_violin( alpha = 0.1) + 
       geom_boxplot(alpha = 0.3, width = 0.1) +
-      # geom_point() +
-      geom_beeswarm(aes(color = interaction(start_mut_prob, prob_mut_probs, gauss, remap, delay)), size = 0.5) +
-      geom_pwc(
-        group.by = "x.var",
-        method = "t_test",
-        label = "{p.adj}{p.adj.signif}",
-        p.adjust.method = "BH",
-        p.adjust.by = "group",
-        hide.ns = F
-      ) +
+      geom_beeswarm(size = 0.5) +
       scale_y_continuous(expand = expansion(mult = c(0, 0.08))) + 
       scale_fill_viridis(discrete = T) +
       scale_color_viridis(discrete = T) +
@@ -112,77 +124,67 @@ add_violin_box_plot<- function(plot){
   )
 }
 
-###wrangle avg fitness data
-wrangle_avg_final_fitness = function(data){
-  return(data %>% 
-           group_by(start_mut_prob, prob_mut_probs, gauss, remap, delay, run) %>% 
-           filter(gen == max(gen)) %>% 
-           summarise(avg_final_fitness = mean(-1 * best_fit)) %>% 
-           ungroup()
+### add t_test comparison bars
+add_t_test = function(plot){
+  return(
+    plot +
+      geom_pwc(
+        group.by = "x.var",
+        method = "t_test",
+        label = "{p.adj}{p.adj.signif}",
+        p.adjust.method = "BH",
+        p.adjust.by = "group",
+        hide.ns = T
+      )
   )
 }
-###wrangle avg fitness data
-wrangle_avg_mid_fitness = function(data){
+
+###wrangle avg fitness data of gen n
+wrangle_avg_fitness_gen = function(data, n_gen){
   return(data %>% 
            group_by(start_mut_prob, prob_mut_probs, gauss, remap, delay, run) %>% 
-           filter(gen == max(gen)/4) %>% 
-           summarise(avg_final_fitness = mean(-1 * best_fit)) %>% 
+           filter(gen == n_gen) %>% 
+           dplyr::summarise(avg_fitness = mean(-1 * best_fit)) %>% 
            ungroup()
   )
 }
 
-###Plot final best fitness 
-make_best_fitness_final_t_test_boxplot = function(data){
-  
-  b = wrangle_avg_final_fitness(data)
+###Plot best fitness boxplot of generation n
+make_best_fitness_t_test_boxplot_gen = function(data, gen){
+
+  b = wrangle_avg_fitness_gen(data, gen)
   p <- b %>% ggplot(aes(x = interaction(start_mut_prob, prob_mut_probs, gauss, remap, delay),
-                        y = avg_final_fitness,
-                        fill = interaction(start_mut_prob, prob_mut_probs, gauss, remap, delay)
+                        y = avg_fitness,
+                        fill = interaction(start_mut_prob, prob_mut_probs, gauss, remap, delay),
+                        color = interaction(start_mut_prob, prob_mut_probs, gauss, remap, delay)
   )
   ) + 
     xlab("parameters combination")+
-    ylab(paste("average best fitness in last generation", sep = ""))
+    ylab(paste("average best fitness in generation ",gen, sep = ""))
   
   p = add_violin_box_plot(p)
   print(p)
-  ggsave(paste("average_fitness_in_last_generation_across_approachs.jpg", sep = ""))
-  ggsave(paste("average_fitness_in_last_generation_across_approachs.pdf", sep = ""),
+  ggsave(paste("average_fitness_in_",gen,"_generation_across_approachs.jpg", sep = ""))
+  ggsave(paste("average_fitness_in_",gen,"_generation_across_approachs.pdf", sep = ""),
          width = 14,
          height = 7,
          units ="cm")
   
+  p_t_test = add_t_test(p)
+  print(p_t_test)
+  ggsave(paste("t_test_average_fitness_in_",gen,"_generation_across_approachs.jpg", sep = ""))
+  
   return(p)
 }
 
-###Plot final best fitness 
-make_best_fitness_mid_t_test_boxplot = function(data){
-  
-  b = wrangle_avg_mid_fitness(data)
-  p <- b %>% ggplot(aes(x = interaction(start_mut_prob, prob_mut_probs, gauss, remap, delay),
-                        y = avg_final_fitness,
-                        fill = interaction(start_mut_prob, prob_mut_probs, gauss, remap, delay)
-  )
-  ) + 
-    xlab("parameters combination")+
-    ylab(paste("average best fitness in last generation", sep = ""))
-  
-  p = add_violin_box_plot(p)
-  print(p)
-  ggsave(paste("average_fitness_in_mid_generation_across_approachs.jpg", sep = ""))
-  ggsave(paste("average_fitness_in_mid_generation_across_approachs.pdf", sep = ""),
-         width = 14,
-         height = 7,
-         units ="cm")
-  return(p)
-}
-
-
+#use when need loading from raw data and not .csv
+#
 data = read_and_save(wd)
+data = read.csv2("data.csv", header = T, sep =",")
 
-data = read.csv("data.csv")
-data = data[-1]
-
-fitness_over_time_across_approachs(data = data)
-make_best_fitness_mid_t_test_boxplot(data)
-fitness_over_time_across_approachs(data)
-
+fitness_over_time_across_approachs(data = data %>% subset(alg_type == "co-psge"))
+fitness_over_time_across_approachs(data = data %>% subset(alg_type == "psge"))
+make_best_fitness_t_test_boxplot_gen(data %>% subset(alg_type == "co-psge"), gen = 250)
+make_best_fitness_t_test_boxplot_gen(data = data %>% subset(alg_type == "co-psge"), gen = 500)
+make_best_fitness_t_test_boxplot_gen(data = data %>% subset(alg_type == "co-psge"), gen = 1000)
+make_best_fitness_t_test_boxplot_gen(data = data %>% subset(alg_type == "psge"), gen = 1000)
