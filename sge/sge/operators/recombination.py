e6b7ddd14aa4664c33e8f076afc9b6ab03477569
jessicamegane <jessicac@dei.uc.pt>
2023-11-03 16:11:54 +0000

    added adaptive mutation code

--- sge/sge/operators/recombination.py
+++ sge/sge/operators/recombination.py
@@ -7,12 +7,20 @@ def crossover(p1, p2):
     gen_size = len(p1['genotype'])
     mask = [numpy.random.uniform() for i in range(gen_size)]
     genotype = []
+    mutation_probs = []
     for index, prob in enumerate(mask):
         if prob < xover_p_value:
             genotype.append(p1['genotype'][index][:])
+            if params['ADAPTIVE_MUTATION']:
+                mutation_probs.append(p1['mutation_probs'][index])
         else:
             genotype.append(p2['genotype'][index][:])
+            if params['ADAPTIVE_MUTATION']:
+                mutation_probs.append(p2['mutation_probs'][index])
     mapping_values = [0] * gen_size
     # compute nem individual
     _, tree_depth = grammar.mapping(genotype, mapping_values)
-    return {'genotype': genotype, 'fitness': None, 'mapping_values': mapping_values, 'tree_depth': tree_depth}
+    if params['ADAPTIVE_MUTATION']:
+        return {'genotype': genotype, 'fitness': None, 'mapping_values': mapping_values, 'tree_depth': tree_depth}
+    else:
+        return {'genotype': genotype, 'fitness': None, 'mapping_values': mapping_values, 'tree_depth': tree_depth, 'mutation_probs': mutation_probs}
