465fe1112da80201e14e7bd19f81c2c493617e8c
jessicamegane <jessicac@dei.uc.pt>
2023-12-19 17:35:52 +0000

    slightly improved mutation code

--- sge/sge/operators/mutation.py
+++ sge/sge/operators/mutation.py
@@ -15,32 +15,36 @@ def mutate(p, pmutation):
         for position_to_mutate in range(0, mapped):
             if np.random.uniform() < pmutation:
                 current_value = p['genotype'][at_gene][position_to_mutate]
+                current_depth = current_value[2]
+                shortest_path = grammar.get_shortest_path()[(nt,'NT')]
+                nt_index = grammar.get_index_of_non_terminal()[nt]
                 # gaussian mutation
-                codon = np.random.normal(current_value[1], 0.5)
-                codon = min(codon,1.0)
-                codon = max(codon,0.0)
-                if p['tree_depth'] >= grammar.get_max_depth():
-                    prob_non_recursive = 0.0
-                    for rule in grammar.get_shortest_path()[(nt,'NT')][1:]:
+                codon = np.clip(np.random.normal(current_value[1], 0.5), 0.0, 1.0)
+                if current_depth >= (grammar.get_max_depth() - shortest_path[0]):
+                    sum_available_prods = 0.0
+                    for rule in shortest_path[1:]:
                         index = grammar.get_dict()[nt].index(rule)
-                        prob_non_recursive += grammar.get_pcfg()[grammar.get_index_of_non_terminal()[nt],index]
+                        sum_available_prods += grammar.get_pcfg()[nt_index,index]
                     prob_aux = 0.0
-                    for rule in grammar.get_shortest_path()[(nt,'NT')][1:]:
+                    for rule in shortest_path[1:]:
                         index = grammar.get_dict()[nt].index(rule)
-                        new_prob = grammar.get_pcfg()[grammar.get_index_of_non_terminal()[nt],index] / prob_non_recursive
+                        if sum_available_prods == 0.0: 
+                            # when the probability of choosing the symbol is 0, we use uniform probabilities
+                            new_prob = 1.0 / len(shortest_path[1:])
+                        else:
+                            new_prob = grammar.get_pcfg()[nt_index,index] / sum_available_prods
                         prob_aux += new_prob
                         if codon <= round(prob_aux,3):
                             expansion_possibility = index
                             break
                 else:
                     prob_aux = 0.0
-                    for index, option in enumerate(grammar.get_dict()[nt]):
+                    for index in range(len(grammar.get_dict()[nt])):
                         prob_aux += grammar.get_pcfg()[grammar.get_index_of_non_terminal()[nt],index]
                         if codon <= round(prob_aux,3):
                             expansion_possibility = index
                             break
-                  
-                p['genotype'][at_gene][position_to_mutate] = [expansion_possibility, codon]
+                p['genotype'][at_gene][position_to_mutate] = [expansion_possibility,codon,current_depth]
     return p
 
 def mutate_level(p):
@@ -56,34 +60,35 @@ def mutate_level(p):
         for position_to_mutate in range(0, mapped):
             if np.random.uniform() < pmutation[at_gene]:
                 current_value = p['genotype'][at_gene][position_to_mutate]
+                current_depth = current_value[2]
+                shortest_path = grammar.get_shortest_path()[(nt,'NT')]
+                nt_index = grammar.get_index_of_non_terminal()[nt]
                 # codon = random.random()
                 # gaussian mutation
-                codon = np.random.normal(current_value[1], 0.5)
-                codon = min(codon,1.0)
-                codon = max(codon,0.0)
+                codon = np.clip(np.random.normal(current_value[1], 0.5), 0.0, 1.0)
                 expansion_possibility = 0
-                if p['tree_depth'] >= grammar.get_max_depth():
+                if current_depth >= grammar.get_max_depth():
                     prob_non_recursive = 0.0
-                    for rule in grammar.get_shortest_path()[(nt,'NT')][1:]:
+                    for rule in shortest_path[1:]:
                         index = grammar.get_dict()[nt].index(rule)
-                        prob_non_recursive += grammar.get_pcfg()[grammar.get_index_of_non_terminal()[nt],index]
+                        prob_non_recursive += grammar.get_pcfg()[nt_index,index]
                     prob_aux = 0.0
-                    for rule in grammar.get_shortest_path()[(nt,'NT')][1:]:
+                    for rule in shortest_path[1:]:
                         index = grammar.get_dict()[nt].index(rule)
-                        new_prob = grammar.get_pcfg()[grammar.get_index_of_non_terminal()[nt],index] / prob_non_recursive
+                        new_prob = grammar.get_pcfg()[nt_index,index] / prob_non_recursive
                         prob_aux += new_prob
                         if codon <= round(prob_aux,3):
                             expansion_possibility = index
                             break
                 else:
                     prob_aux = 0.0
-                    for index, option in enumerate(grammar.get_dict()[nt]):
-                        prob_aux += grammar.get_pcfg()[grammar.get_index_of_non_terminal()[nt],index]
+                    for index in range(len(grammar.get_dict()[nt])):
+                        prob_aux += grammar.get_pcfg()[nt_index,index]
                         if codon <= round(prob_aux,3):
                             expansion_possibility = index
                             break
                   
-                p['genotype'][at_gene][position_to_mutate] = [expansion_possibility, codon]
+                p['genotype'][at_gene][position_to_mutate] = [expansion_possibility,codon,current_depth]
     return p
 
 
